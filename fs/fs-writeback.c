c94c4f5be66861aab2e8fe170962058159a0f798
Brooke Spencer <xXminiWHOOPERxX@github.com>
2015-03-22 22:56:19 -0400

    sync: dont block the flusher thread waiting on IO

--- fs/fs-writeback.c
+++ fs/fs-writeback.c
@@ -40,7 +40,8 @@ struct wb_writeback_work {
 	unsigned int for_kupdate:1;
 	unsigned int range_cyclic:1;
 	unsigned int for_background:1;
-	enum wb_reason reason;		
+	unsigned int for_sync:1;
+	enum wb_reason reason;	
 
 	struct list_head list;		
 	struct completion *done;	
@@ -296,7 +297,7 @@ writeback_single_inode(struct inode *inode, struct bdi_writeback *wb,
 
 	ret = do_writepages(mapping, wbc);
 
-	if (wbc->sync_mode == WB_SYNC_ALL) {
+	if (wbc->sync_mode == WB_SYNC_ALL && !wbc->for_sync) {
 		int err = filemap_fdatawait(mapping);
 		if (ret == 0)
 			ret = err;
@@ -375,6 +376,7 @@ static long writeback_sb_inodes(struct super_block *sb,
 		.tagged_writepages	= work->tagged_writepages,
 		.for_kupdate		= work->for_kupdate,
 		.for_background		= work->for_background,
+                .for_sync		= work->for_sync,
 		.range_cyclic		= work->range_cyclic,
 		.range_start		= 0,
 		.range_end		= LLONG_MAX,
@@ -928,6 +930,7 @@ void sync_inodes_sb(struct super_block *sb)
 		.range_cyclic	= 0,
 		.done		= &done,
 		.reason		= WB_REASON_SYNC,
+		.for_sync	= 1,
 	};
 
 	WARN_ON(!rwsem_is_locked(&sb->s_umount));
